b196a16e08b0fd5499e05673682873b7b711d6c0
giedrius-tumosa <gie.tumosa@gmail.com>
2023-01-09 13:50:33 +0200

    drafted Tab_modal.js class

---
+++ scripts/Tab_modal.js
@@ -0,0 +1,28 @@
+import Tab_card from "./Tab_card.js";
+
+export default class Tab_modal {
+  constructor(cardData) {
+    this.cardData = cardData;
+    return this.render();
+  }
+  render() {
+    const pageWrapper = document.querySelector("#page_wrapper");
+    const modalBackground = document.createElement("div");
+    modalBackground.setAttribute("class", "modal-background");
+
+    const card = new Tab_card(this.cardData);
+    modalBackground.append(card);
+    pageWrapper.append(modalBackground);
+
+    // const widthBefore = document.querySelector("html").getBoundingClientRect().width;
+
+    // const body = document.querySelector("body");
+
+    // const widthAfter = document.querySelector("html").getBoundingClientRect().width;
+
+
+    return modalBackground;
+
+  }
+
+}

7327ea598002b95e5a4c83a39f5800c3d3298413
giedrius-tumosa <gie.tumosa@gmail.com>
2023-02-01 09:11:00 +0200

    fixed image links

--- scripts/data.js
+++ scripts/data.js
@@ -1,25 +1,25 @@
 export const tabData = [
   {
     title: "intro",
-    mediaUrl: "../images/tab-1.mp4",
+    mediaUrl: "./images/tab-1.mp4",
     mediaType: "video",
     description: `Lorem ipsum dolor sit amet, in nec nonumes volutpat, nec delectus reprimique an. An eam saepe quaeque albucius. Id consul possim maluisset usu. Ea meis civibus mei, vel ad oratio tibique. Alienum necessitatibus ut qui, inermis iudicabit constituto ne sea. Postea doctus audiam quo no, id cum mentitum voluptaria.`
   },
   {
     title: "work",
-    mediaUrl: "../images/tab-2.jpg",
+    mediaUrl: "./images/tab-2.jpg",
     mediaType: "image",
     description: `Lorem ipsum dolor sit amet, in nec nonumes volutpat, nec delectus reprimique an. An eam saepe quaeque albucius. Id consul possim maluisset usu. Ea meis civibus mei, vel ad oratio tibique. Alienum necessitatibus ut qui, inermis iudicabit constituto ne sea. Postea doctus audiam quo no, id cum mentitum voluptaria.`
   },
   {
     title: "about",
-    mediaUrl: "../images/tab-3.jpg",
+    mediaUrl: "./images/tab-3.jpg",
     mediaType: "image",
     description: `Lorem ipsum dolor sit amet, in nec nonumes volutpat, nec delectus reprimique an. An eam saepe quaeque albucius. Id consul possim maluisset usu. Ea meis civibus mei, vel ad oratio tibique. Alienum necessitatibus ut qui, inermis iudicabit constituto ne sea. Postea doctus audiam quo no, id cum mentitum voluptaria.`
   },
   {
     title: "contact",
-    mediaUrl: "../images/tab-4.jpg",
+    mediaUrl: "./images/tab-4.jpg",
     mediaType: "image",
     description: `Lorem ipsum dolor sit amet, in nec nonumes volutpat, nec delectus reprimique an. An eam saepe quaeque albucius. Id consul possim maluisset usu. Ea meis civibus mei, vel ad oratio tibique. Alienum necessitatibus ut qui, inermis iudicabit constituto ne sea. Postea doctus audiam quo no, id cum mentitum voluptaria.`
   }
